116b14b18d65f48a0a2ca40346df554b2a0a1825
Geoffrey87 <lloydcosta@hotmail.com>
2024-12-09 15:23:38 +0000

    Create ToDo form

--- TypeScript/MiniProject/ToDoProject/src/main.ts
+++ TypeScript/MiniProject/ToDoProject/src/main.ts
@@ -1,3 +1,23 @@
 import { elements } from "./utils/elements";
+import type { Todo } from "./types/todos";
 
-console.log(elements);
+const todos: Todo[] = [];
+
+function hideError(): void {
+  elements.errorPopup.container.classList.add("hidden");
+}
+
+function showError(message: string): void {
+  elements.errorPopup.container.classList.remove("hidden");
+  elements.errorPopup.message.textContent = message;
+}
+
+elements.form.onsubmit = (event) => {
+  event.preventDefault();
+  const value = elements.todoInput.value.trim();
+  if (value === "") {
+    showError("Todo cannot be empty");
+    return;
+  }
+  elements.errorPopup.closeButton.onclick = hideError;
+};
