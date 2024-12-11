cdaf973e09ef70169f4f6ba4746d04d73d3a24be
Geoffrey87 <lloydcosta@hotmail.com>
2024-12-11 16:04:17 +0000

    create XMLHttpRequest

--- TypeScript/WhereInTheWorld/whereInTheWorld/src/main.ts
+++ TypeScript/WhereInTheWorld/whereInTheWorld/src/main.ts
@@ -2,3 +2,20 @@ import "./styles.css";
 import { elements } from "./utils/elements";
 
 elements;
+
+elements.form.onsubmit = (e) => {
+  //e of event
+  e.preventDefault();
+
+  const input = elements.searchInput.value.trim();
+  const request = new XMLHttpRequest();
+
+  request.open("GET", `https://restcountries.com/v3.1/name/${input}`);
+  request.onload = () => {
+    console.log(JSON.parse(request.response));
+  };
+  request.onerror = () => {
+    console.log("error");
+  };
+  request.send();
+};
