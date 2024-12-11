42452179ef2995e6355b925d2b5c64fe5d3aefb5
Geoffrey87 <lloydcosta@hotmail.com>
2024-12-11 16:42:25 +0000

    create promise for the Get request

---
+++ TypeScript/WhereInTheWorld/whereInTheWorld/src/api/xml-get-countries.ts
@@ -0,0 +1,17 @@
+export function xmlGetCountries(nameQuery: string) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.com/v3.1/name/${nameQuery}`);
+    request.onload = () => {
+      if (request.status === 200) {
+        const countries = JSON.parse(request.response);
+        resolve(countries);
+      } else {
+        const error = JSON.parse(request.response);
+        reject(error);
+      }
+    };
+
+    request.send();
+  });
+}
--- TypeScript/WhereInTheWorld/whereInTheWorld/src/main.ts
+++ TypeScript/WhereInTheWorld/whereInTheWorld/src/main.ts
@@ -1,21 +1,20 @@
+import { xmlGetCountries } from "./api/xml-get-countries";
 import "./styles.css";
 import { elements } from "./utils/elements";
-
-elements;
 
 elements.form.onsubmit = (e) => {
   //e of event
   e.preventDefault();
 
   const input = elements.searchInput.value.trim();
-  const request = new XMLHttpRequest();
-
-  request.open("GET", `https://restcountries.com/v3.1/name/${input}`);
-  request.onload = () => {
-    console.log(JSON.parse(request.response));
-  };
-  request.onerror = () => {
-    console.log("error");
-  };
-  request.send();
+  xmlGetCountries(input)
+    .then((countries) => {
+      console.log(countries);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      console.log("Here!");
+    });
 };
